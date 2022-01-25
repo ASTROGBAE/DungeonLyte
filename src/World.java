32f426bcfc511b5d0d0947e76b03cd2699cf1a6a
ASTROGBAE <gabebrett@iinet.net.au>
2022-01-25 11:02:58 +1100

    changed ExpFormat and make Drake class objects for each read line, added better header precedence function

--- src/World.java
+++ src/World.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -7,46 +8,52 @@ import java.util.Scanner;
 public class World {
 
     private Map<String, Room> rooms;
-    private ExpFormat[] headers = {new ExpFormat("ROOM"), new ExpFormat("DOOR")};
+    private ArrayList<String> headers = new ArrayList<String>();
 
     public World() {
         rooms = new HashMap<String, Room>();
+        String[] heads = {"Room", "Door"}; for (String s : heads) {headers.add(s);} // scan in head objects
         loadGame(); // load in game objects
     }
 
     // return file to be scanned  
     private void loadGame() {
         try { FileInputStream gameStream =new FileInputStream("data/game.txt");
-            for(ExpFormat h : headers) { Scanner gameScan = new Scanner(gameStream); // perform a seperate scan per header object
-                scanHeaders(h, gameScan);
+            for(String header : headers) { Scanner gameScan = new Scanner(gameStream); // perform a seperate scan per header object
+                levelScan(header, gameScan);
                 gameScan.close();     //closes the scanner 
             }
         }
         catch(IOException e)  {e.printStackTrace();}  
     }
     
-    // scan in objects from scan according to the provided head object 
-    // TODO find out why an empty room is build first??
-    private void scanHeaders(ExpFormat head, Scanner _gameScan) {
+    // scan game at a specific header level, remembering the last head in precedence (for linear referencing purposes)
+    private void levelScan(String level, Scanner _gameScan) {
         //returns true if there is another line to read
         while(_gameScan.hasNextLine())  { // scanning for rooms
-            String line = _gameScan.nextLine();
-            String lastRoomTitle = "";
+            Drake curDrake = new Drake(_gameScan.nextLine()); // create drake object for current level
+            String lastHigherTitle = "";
 
-            if (head.getHead() == "ROOM") { // TODO find out why there is an empty first entry?
-                String title = head.getTitle(line);
-                String tail = head.getTail(line);
-                rooms.put(title, new Room(title, tail)); // TODO refactor usage of title?
+            if (curDrake.compareToHeader(level, headers) < 0) { // if scan of current drake is higher than the level, get title of currrent drake for further reference
+                // order in terms of precedence, perhaps?
+                lastHigherTitle = curDrake.getTitle();
             }
-            else if (head.getHead() == "DOOR") {
-                if (headers[0].isExpression(line)) { // if room expression but door scan, then remember last room
-                    // TODO refactor so this is if exp is cleaner? make each format a variable perhaps?
-                    lastRoomTitle = headers[0].getTitle(line);
-                }
-                Room[] link = {rooms.get(lastRoomTitle), rooms.get(head.getTitle(line))}; // get last room and subject room from title
-                Door _door = new Door(head.getTail(line), link); // create door instance 
-                for (Room r : link) {r.addDoor(_door);} // add door ref to both rooms
-            }
+            toDungeonObject (curDrake, lastHigherTitle); // create dungeon item
+        }
+    }
+
+    // takes string and format parameters for each value and creates an appopriate dungeon object (not returned)
+    private void toDungeonObject (Drake drakeToConvert, String lastTitle) {
+        String head = drakeToConvert.getHead();
+        String title = drakeToConvert.getTitle();
+        String tail = drakeToConvert.getTail();
+        if (head == "Room") {
+            rooms.put(title, new Room(title, tail)); // TODO refactor usage of title?
+        }
+        else if (head == "Door") {
+            Room[] link = {rooms.get(lastTitle), rooms.get(title)}; // get last room and subject room from title
+            Door _door = new Door(tail, link); // create door instance 
+            for (Room r : link) {r.addDoor(_door);} // add door ref to both rooms
         }
     }
 
