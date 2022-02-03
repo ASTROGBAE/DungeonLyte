d0906171ae00b17ec3de719583d949343e24a82e
GBAE <gabebrett@iinet.net.au>
2022-02-03 12:20:01 +1100

    comments to header, complete later

--- src/Header.java
+++ src/Header.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Header {
+    // TODO integrate header objects into code (not yet done, class unused)
     String head;
     String[] headArr = {"Room", "Door", "Item", "Feature", "Lock"};
     ArrayList<String> headerList;
